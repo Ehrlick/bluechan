a58266be3ad8fd9fab1e7ca32b367a428458fead
Cmflgee <camouflagee@gamil.com>
2012-10-05 15:32:43 +0900

    add university

--- app/Controller/UniversitiesController.php
+++ app/Controller/UniversitiesController.php
@@ -6,4 +6,16 @@ class UniversitiesController extends AppController {
 	public function index() {
 		$this->set('universities', $this->University->find('all'));
 	}
+	
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->University->create();
+			if ($this->University->save($this->request->data)) {
+				$this->Session->setFlash('new University has been saved.');
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash('Unable to add the University.');
+			}
+		}
+	}
 }
